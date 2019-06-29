e7b0fb3e92cdd29071f8df4dc6f62320dd5f31cd
lxy <1941389494@qq.com>
2019-06-29 22:17:16 +0800

    第一次提交java项目

---
+++ demo-test/src/main/java/cn/itcast/test/DemoTest.java
@@ -0,0 +1,12 @@
+package cn.itcast.test;
+
+/**
+ * Author: Li.xu.yang
+ * Date: 2019/6/29 22:04
+ * Content:
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("1111-444-5555");
+    }
+}
